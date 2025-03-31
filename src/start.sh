23299db5f38c951be8a7ef189995960e4047055f
risunobushi <113669885+risunobushi@users.noreply.github.com>
2025-03-31 12:11:43 +0200

    update to force extra models

--- src/start.sh
+++ src/start.sh
@@ -6,14 +6,14 @@ export LD_PRELOAD="${TCMALLOC}"
 
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata --listen &
+    echo "runpod-worker-comfy: Starting ComfyUI with explicit config"
+    python3 /comfyui/main.py --extra-model-paths-config /comfyui/extra_model_paths.yaml --disable-auto-launch --disable-metadata --listen &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py --rp_serve_api --rp_api_host=0.0.0.0
 else
-    echo "runpod-worker-comfy: Starting ComfyUI"
-    python3 /comfyui/main.py --disable-auto-launch --disable-metadata &
+    echo "runpod-worker-comfy: Starting ComfyUI with explicit config"
+    python3 /comfyui/main.py --extra-model-paths-config /comfyui/extra_model_paths.yaml --disable-auto-launch --disable-metadata &
 
     echo "runpod-worker-comfy: Starting RunPod Handler"
     python3 -u /rp_handler.py
